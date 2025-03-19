8755158f438652c98956800a400aa307cc8dc6ef
Marcoandres <marcoandresha@gmail.com>
2025-03-19 16:28:25 -0500

    cambios de mejora

--- src/app/page.js
+++ src/app/page.js
@@ -14,58 +14,157 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 
+
+
 export default function Home() {
   return (
-    <div >
-      <div className="flex flex-row justify-center align-middle bg-[#5c60dc] py-[180px]">
-        <div className="w-1/2">
-          <h1 className="text-5xl text-white">Toma el control de tus finanzas con AdminCash</h1>
+    <div>
+      <div className="flex flex-row justify-center align-middle bg-[#5c60dc] py-[130px]">
+        <div className=" flex flex-col justify-center align-middle w-1/2 ">
+          <h1 className="text-5xl text-white font-bold">Toma el control de tus finanzas con AdminCash</h1>
           <h2>Administra tus ingresos, gastos y ahorros de manera fácil e inteligente.</h2>
         </div>
     
         <img className="w-[400px] w-1/2" src="https://cdn.prod.website-files.com/640f69143ec11b21d42015c6/67aa6769519aa08294a66fce_features_hero_test%20(1).avif" alt=""/>
       </div>
       
-      
       <div className="text-center py-20 px-30">
         <h3 className="text-4xl">&quot;Simplifica tus finanzas, controla tus gastos y alcanza tus metas fácilmente.&quot;</h3>
       </div>
       <div>
-        <h3 className="text-4xl">¿Por qué elegir AdminCash?</h3>
-        <ul>
-          <li>✅ Gestión de ingresos y gastos → Registra y categoriza todos tus movimientos.</li>
-          <li>✅ Presupuestos personalizados → Planifica tus gastos según tus necesidades.</li>
-          <li>✅ Reportes y análisis → Obtén gráficos detallados sobre tus finanzas.</li>
-          <li>✅ Seguridad garantizada → Tus datos están protegidos con encriptación avanzada.</li>
-          <li>✅ Accesible en cualquier dispositivo → Disponible en web y móvil.</li>
+        <div>
+          <h3 className="text-4xl">¿Por qué elegir AdminCash?</h3>
+          <p>
+            AdminCash te ofrece todas las herramientas que necesitas para tomar el control de tus finanzas de manera sencilla y eficiente. Descubre cómo nuestra 
+            plataforma puede ayudarte a gestionar tus ingresos, optimizar tus presupuestos y visualizar tu progreso con reportes detallados. 🚀
+          </p>
+        </div>
+        
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-10">
+          <li className="w-[300px]">
+            <div className="flex justify-center border-1 border-b-blue-950 p-5">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 256 257" version="1.1" role="img">
+                <g stroke="none" strokeWidth="1" fillRule="evenodd">
+                <path d="M0,128.002 C0,180.416 31.518,225.444 76.619,245.241 C76.259,236.303 76.555,225.573 78.847,215.848 C81.308,205.457 95.317,146.1 95.317,146.1 C95.317,146.1 91.228,137.927 91.228,125.848 C91.228,106.879 102.222,92.712 115.914,92.712 C127.557,92.712 133.182,101.457 133.182,111.929 C133.182,123.633 125.717,141.14 121.878,157.355 C118.671,170.933 128.686,182.008 142.081,182.008 C166.333,182.008 182.667,150.859 182.667,113.953 C182.667,85.899 163.772,64.901 129.405,64.901 C90.577,64.901 66.388,93.857 66.388,126.201 C66.388,137.353 69.676,145.217 74.826,151.307 C77.194,154.104 77.523,155.229 76.666,158.441 C76.052,160.796 74.642,166.466 74.058,168.713 C73.206,171.955 70.579,173.114 67.649,171.917 C49.765,164.616 41.436,145.031 41.436,123.015 C41.436,86.654 72.102,43.054 132.918,43.054 C181.788,43.054 213.953,78.418 213.953,116.379 C213.953,166.592 186.037,204.105 144.887,204.105 C131.068,204.105 118.069,196.635 113.616,188.15 C113.616,188.15 106.185,217.642 104.611,223.337 C101.897,233.206 96.585,243.07 91.728,250.758 C103.24,254.156 115.401,256.007 128.005,256.007 C198.689,256.007 256.001,198.698 256.001,128.002 C256.001,57.309 198.689,0 128.005,0 C57.314,0 0,57.309 0,128.002 Z"></path>                 
+                </g>
+              </svg>
+            </div>
+            <div className="bg-[#262973] text-white p-5">
+              <h2 className="font-bold">✅ Gestión de ingresos y gastos </h2>
+              <p>Registra y categoriza todos tus movimientos.</p>
+            </div>
+          </li>
+          <li className="w-[300px]">
+            <div className="flex justify-center border-1 border-b-blue-950 p-5">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 256 257" version="1.1" role="img">
+
+              </svg>
+            </div>
+            <div className="bg-[#262973] text-white p-5">
+              <h2 className="font-bold">✅ Presupuestos personalizados</h2>
+              <p>Planifica tus gastos según tus necesidades.</p>
+            </div>
+          </li>
+          <li className="w-[300px]">
+            <div className="flex justify-center border-1 border-b-blue-950 p-5">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 256 257" version="1.1" role="img">
+
+              </svg>
+            </div>
+            <div className="bg-[#262973] text-white p-5">
+              <h2 className="font-bold">✅ Reportes y análisis</h2>
+              <p>Obtén gráficos detallados sobre tus finanzas.</p>
+            </div>
+          </li>
+          <li className="w-[300px]">
+            <div className="flex justify-center border-1 border-b-blue-950 p-5">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 256 257" version="1.1" role="img">
+
+              </svg>
+            </div>
+            <div className="bg-[#262973] text-white p-5">
+              <h2 className="font-bold">✅ Seguridad garantizada</h2>
+              <p>Tus datos están protegidos con encriptación avanzada.</p>
+            </div>
+          </li>
+          <li className="w-[300px]">
+            <div className="flex justify-center border-1 border-b-blue-950 p-5">
+              <svg xmlns="http://www.w3.org/2000/svg" width="50px" height="50px" viewBox="0 0 256 257" version="1.1" role="img">
+
+              </svg>
+            </div>
+            <div className="bg-[#262973] text-white p-5">
+              <h2 className="font-bold">✅ Accesible en cualquier dispositivo</h2>
+              <p>Disponible en web y móvil.</p>
+            </div>
+          </li>
         </ul>
       </div>
     
-    <div className="flex flex-col justify-center items-center">
-      <h2>Características</h2>
-      <Carousel>
-        <CarouselContent>
-          <CarouselItem className="basis-1/2 lg:basis-1/3 ">Mejora manejo finaziero</CarouselItem>
-          <CarouselItem className="basis-1/2 lg:basis-1/3">...</CarouselItem>
-          <CarouselItem className="basis-1/2 lg:basis-1/3">...</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
+      <div className="flex flex-col justify-center items-center">
+        <h2>Características</h2>
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem className="basis-1/2 lg:basis-1/3">Mejora manejo finaziero</CarouselItem>
+            <CarouselItem className="basis-1/2 lg:basis-1/3">...</CarouselItem>
+            <CarouselItem className="basis-1/2 lg:basis-1/3">...</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
 
-    <div>
-      <h2>Preguntas frecuentes</h2>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <div className="flex flex-row p-10 h-[650px] ">
+        <div className="flex flex-col align-middle justify-center">
+          <img src="/home/img-duda.jpg" alt=""/>
+        </div>
+        <div className="flex flex-col align-middle justify-center">
+          <h2 className="text-center text-2xl p-2">Preguntas frecuentes</h2>
+          <div className="flex flex-col w-[500px] ">
+            <Accordion className="border-1 border-black" type="single" collapsible >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="px-4">1. ¿Qué es AdminCash y cómo me ayuda?</AccordionTrigger>
+                <AccordionContent className="px-4">
+                AdminCash es una plataforma de gestión financiera que te permite controlar tus ingresos, gastos y presupuestos en un solo lugar. 
+                Con herramientas intuitivas y reportes detallados, podrás mejorar tu administración del dinero y alcanzar tus metas financieras.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+
+            <Accordion className="border-1 border-black" type="single" collapsible >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="px-4">2. ¿AdminCash es gratis?</AccordionTrigger>
+                <AccordionContent className="px-4">
+                AdminCash ofrece un plan gratuito con funciones esenciales para la gestión de tus finanzas. También contamos con planes premium que 
+                desbloquean herramientas avanzadas como reportes personalizados, sincronización con cuentas bancarias y más.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+
+            <Accordion className="border-1 border-black" type="single" collapsible >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="px-4">3. ¿Necesito conocimientos financieros para usar AdminCash?</AccordionTrigger>
+                <AccordionContent className="px-4">
+                No, AdminCash es fácil de usar incluso si no tienes experiencia previa en finanzas. Nuestra interfaz intuitiva y nuestras guías te 
+                ayudarán a administrar tu dinero sin complicaciones.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+
+            <Accordion className="border-1 border-black" type="single" collapsible >
+              <AccordionItem value="item-1">
+                <AccordionTrigger className="px-4">4. ¿Puedo acceder a AdminCash desde mi teléfono?</AccordionTrigger>
+                <AccordionContent className="px-4">
+                Sí, AdminCash está disponible en versión web y móvil, por lo que puedes gestionar tus finanzas desde cualquier dispositivo con 
+                conexión a internet.
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        </div>
+        
+        
       </div>
     </div>
-
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,6 +12,7 @@ import {
   import { Button } from "@/components/ui/button"
 
   import Link from "next/link"
+
   import { cn } from "@/lib/utils";
 
 
@@ -56,22 +57,34 @@ const Nav = () => {
   ]
   return (
     <>
-    <div className='flex justify-between items-center bg-[#262973] text-white p-4 w-full'>
-        <h1>AdminCash</h1>
+      <div className='flex justify-between items-center bg-[#1D8647] text-white p-4 w-full'>
+        <a href="/">
+          <div className='flex items-center'>
+            <svg className='w-[30px]' xmlns="http://www.w3.org/2000/svg" version="1.0" width="42.0pt" height="42.0pt" viewBox="0 0 42.0 42.0" preserveAspectRatio="xMidYMid meet">
+              <g transform="translate(0.000000,42.000000) scale(0.100000,-0.100000)" fill="#f1f1f1" stroke="none">
+                <path d="M305 368 c-60 -21 -94 -73 -79 -120 6 -19 6 -19 30 3 13 12 25 34 25 48 1 18 3 20 6 8 4 -14 8 -16 19 -6 8 7 14 7 14 1 0 -5 -8 -13 -17 -17 -17 -7 -16 -8 2 -15 18 -8 17 -9 -7 -9 -27 -1 -62 -36 -52 -53 10 -16 53 -7 83 17 26 22 71 115 71 145 0 13 -57 11 -95 -2z"/>
+                <path d="M67 295 c-52 -18 -54 -21 -26 -51 34 -37 72 -54 118 -54 73 0 81 69 11 105 -35 18 -53 18 -103 0z m119 -67 c-14 -9 -32 -10 -23 -2 3 4 -4 15 -16 25 -22 19 -22 19 14 2 23 -11 32 -21 25 -25z"/>
+                <path d="M231 184 c-1 -14 -10 -24 -25 -28 -14 -3 -28 -16 -32 -29 -4 -12 -16 -28 -26 -36 -12 -8 -18 -21 -15 -32 4 -13 -2 -19 -22 -22 -14 -2 44 -4 129 -4 85 -1 147 1 137 4 -13 3 -17 11 -14 23 6 22 -18 60 -39 60 -8 0 -23 9 -34 20 -11 11 -26 20 -34 20 -8 0 -17 10 -19 23 -4 21 -4 21 -6 1z"/>
+              </g>
+            </svg>
+            <h1 className='text-3xl font-medium p-2'>AdminCash</h1>
+          </div>
+        </a>
+        
         <NavigationMenu>
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Iniciando</NavigationMenuTrigger>
               <NavigationMenuContent>
                 <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3">
                     <NavigationMenuLink asChild>
                       <a
                         className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
+                        href="/que-es-adminCash"
                       >
                         <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
+                             ¿Que es adminCash?
                         </div>
                         <p className="text-sm leading-tight text-muted-foreground">
                           Beautifully designed components built with Radix UI and
@@ -118,15 +131,11 @@ const Nav = () => {
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-        <div>
-          <Button className='bg-[#4DA838] text-black rounded-md px-8 py-2'>Log in</Button>
-          <button className='bg-[#FFD700] text-black rounded-md py-2'>Sign In</button>
-        </div>
-        
-
-    </div>
-    
-
+        <Link href="/" className="text-white">
+          Log in
+        </Link>
+          <button className='bg-[#FFD700] text-black rounded-md py-2 ml-2'>Sign In</button>
+      </div>
     </>
   )
 }
